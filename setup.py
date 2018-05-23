54ae40a925a983985a1b5689b810cf826e00f303
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-05-24 01:57:27 +0900

    :tada: Release v0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name = 'fobs',
-      description = 'convert 45m/ASTE obstable to one for FMLO observation',
-      version = '0.1',
+      description = 'Convert 45m/ASTE obstable to one for FMLO observation',
+      version = '0.2',
       author = 'astropenguin',
       author_email = 'taniguchi@a.phys.nagoya-u.ac.jp',
       url = 'https://github.com/fmlo-dev/fobs',
